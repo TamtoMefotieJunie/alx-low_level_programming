30529cb9938aa67bded49ea9809726528a97b11f
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-03-10 13:51:18 +0100

    compile

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #!/bin/bash
-int main(){
+include <stdio.h>
+int main() {
 puts("Programming is like building a multilingual puzzle"); 
-return 0;}
+return 0; }
+gcc 4-puts.c -o 4-puts.c
+./4-puts.c
+
