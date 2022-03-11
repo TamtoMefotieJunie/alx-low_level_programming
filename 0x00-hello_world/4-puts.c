ce1be58979969f6e1a231b40157465c1e4f8034e
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-03-11 22:45:58 +0100

    message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/*
+* Description: main - prints out a string to stdout.
+* Return: 0 if success.
+*/
 int main(void) 
 {
-puts("Programming is like building a multilingual puzzle"); 
+puts("\"Programming is like building a multilingual puzzle"); 
 return (0); 
 }
 
