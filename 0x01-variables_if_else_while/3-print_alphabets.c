9ab2d9c56f9d10bb081c06bf85db07adbfd94c3f
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-03-12 21:44:30 +0100

    message

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,9 +12,11 @@ char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 {
 putchar(ch);
-for (ch = 'A'; ch <= 'Z'; ch++)
-putchar(ch);
-putchar('\n');
 }
+for (ch = 'A'; ch <= 'Z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
 return (0);
 }
