e2d287be4692c1fb710a52b9d17402761b3fa1ad
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-03-12 04:01:05 +0100

    message

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+
 /**
- * Description: main - prints out a string to stdout.
- * Return: 0 if success. 
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
  */
 int main(void)
 {
