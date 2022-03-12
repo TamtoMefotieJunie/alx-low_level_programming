90ba05927b0e4179dde7f53c354185f98f234469
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-03-12 22:57:12 +0100

    message

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int ch;
+for (ch = '0'; ch <= '9'; ch++)
+{
+putchar(ch);
+}
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
