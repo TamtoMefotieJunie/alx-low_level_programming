fca33d35306044ba5450ff4cf5862ad59d69c58e
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-03-12 20:26:44 +0100

    message

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,12 +23,12 @@ if (ld == 0)
 {
 printf("Last digit of %i is %i and is 0\n", n, ld);
 }
-}
 else
 {
 if (ld < 6 && ld != 0)
 {
 printf("Last digit of %i is %i and is less than 6 and not 0\n", n);
+}
 }
 }
 return (0);
