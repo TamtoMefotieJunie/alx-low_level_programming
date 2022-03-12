faeb6e44d4d8a7a7205f10ff9e8bfcdd03f1a010
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-03-12 21:52:00 +0100

    message

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; && ch != 'e' && ch != 'q' ch++)
+putchar(ch);
+putchar('\n');
+return (0);
