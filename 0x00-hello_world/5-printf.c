db20314ea150e5f6eb97538234b40381288314c0
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-03-12 03:00:05 +0100

    message

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * Description: main - prints out a string to stdout.
+ *
  * Return: 0 if success. 
  */
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");                  
-        return (0);                                                         
+printf("with proper grammar, but the outcome is a piece of art,\n");                  
+return (0);                                                         
 }
