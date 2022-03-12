f3c2f2d74261441128137a32936dda24ac0da09b
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-03-12 22:11:47 +0100

    message

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i<= 9; i++)
+{
+printf("%d\n", i);
+}
+return (0);
+}
