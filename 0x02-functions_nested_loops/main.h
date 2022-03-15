9affdb7967f89cfa42be9f6b9b6c079e2604a33a
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-03-15 22:56:32 +0100

    loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,2 @@
-#include "main.h"
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-void print_alphabet(void)
-{
-char ch;
-for (ch = 'a'; ch <= 'z'; ch++)
-_putchar(ch);
-_putchar('\n');
-}
+int _putchar(char c);
+void print_alphabet(void);
