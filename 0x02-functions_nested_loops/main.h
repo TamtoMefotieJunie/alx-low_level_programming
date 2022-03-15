0b9593c98d3ba2b44ff5b8b27fa8f17c0e138008
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-03-15 22:46:15 +0100

    loops

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
