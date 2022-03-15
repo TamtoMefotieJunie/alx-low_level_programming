6d62c9d3343cdf5812912fe189a40f467c75511b
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-03-15 20:23:06 +0100

    My loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,13 +4,6 @@
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)
-{
-char ch;
-for (ch = 'a'; ch <= 'z'; ch++)
-_putchar(ch);
-_putchar('\n');
-}
 int main(void)
 {
 print_alphabet();
