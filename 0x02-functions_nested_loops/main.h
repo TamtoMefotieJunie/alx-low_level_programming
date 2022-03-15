b21f607e2aa51461b68bf29d81fb251bede9cc08
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-03-15 20:22:09 +0100

    My loops

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
