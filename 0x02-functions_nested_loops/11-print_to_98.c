853e398e398a7ce83c0ad04a7261d58be9a9fcf4
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-03-16 13:58:03 +0100

    loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
  * File: 11-print_to_98.c
  * Auth: Brennan D Baraban
  */
-#include "main.h"
+#include <stdio.h>
 /**
  * print_to_98 - Prints all natural numbers from input to 98,
  *               in order separated by a comma followed by a space.
