f4d2eaf49f6345ef7e19729a721cd502de3dca38
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-03-16 15:31:19 +0100

    loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,15 +8,21 @@
  */
 void times_table(void);
 {
-int i,j;
-for (i = 0; i <= 9; i++)
+int num, mult, prod;
+for (num = 0; num <= 9; num++)
 {
-for (j = 0; j <= 9; j++)
+_putchar('0');
+for (mult = 1; mult <= 9; mult++)
 {
-if (j <= 8)
-printf("%d, ", i * j);
+_putchar(',');
+_putchar(' ');
+prod = num * mult;
+if (prod <= 9)
+_putchar(' ');
 else
-printf("%d", i * j);
+_putchar((prod / 10) + '0');
+_putchar((prod % 10) + '0');
 }
+_putchar('\n');
 }
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,2 @@
 int _putchar(char c);
-void jack_bauer(void);
+void times_table(void);
