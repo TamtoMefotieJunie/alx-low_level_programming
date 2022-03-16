5c457926f12dbf1adf8e19e67511c1b9eee61db0
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-03-16 13:45:28 +0100

    loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -17,17 +17,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-printf("+");
 return (1);
+printf("+");
 }
 else if (n == 0)
 {
-printf("0");
 return (0);
+printf("0");
 }
-else
+else if (n < 0)
 {
-printf("-");
 return (-1);
+printf("-");
 }
 }
