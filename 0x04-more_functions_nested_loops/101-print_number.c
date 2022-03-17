ecb3b42b7a390f61bb5ac7a918625e3892154add
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-03-17 22:20:52 +0100

    My functions

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+/*
+ * File: 101-print_number.c
+ * Auth: Brennan D Baraban
+ */
+#include "main.h"
+/**
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
+ */
+void print(int n)
+{
+if (n < 0)
+{
+putchar('-');
+n = -n
+}
+if (n / 10 != 0)
+print(n / 10);
+putchar((n % 10) + '0');
+}
