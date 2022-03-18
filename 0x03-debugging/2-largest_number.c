0cd30473f6d1f56d97fd5ab4f18ad486330becfc
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-03-18 22:37:50 +0100

    bugs

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,8 +10,8 @@
 
 int largest_number(int a, int b, int c)
 {
-
 int largest;
+
 if (a >= b && a > c)
 {
 largest = a;
@@ -25,5 +25,5 @@ else
 largest = c;
 }
 
- return (largest);
+return (largest);
 }
