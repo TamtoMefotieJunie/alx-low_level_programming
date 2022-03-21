f4f86f9ca8c40b7580944c9033ac14f8620fa800
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-03-21 15:19:37 +0100

    pointers

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,12 +7,6 @@ void positive_or_negative(int i);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-void _puts(char *str);
-char *_strcpy(char *dest, char *src);
-void print_array(int *a, int n);
-void puts_half(char *str);
 void puts2(char *str);
-void rev_string(char *s);
-void print_rev(char *s);
 
 #endif /* MAIN_H */
