c4d525af9254901df9a1656202afa50deb35ce4f
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-03-21 12:41:54 +0100

    pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+/*
+ * File: 2-strlen.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @str: The string to get the length of.
+ *
+ * Return: The length of @str.
+ */
+int _strlen(char *s)
+{
+int l = 0;
+while (*s != '\0')
+l++
+return (l);
+}
