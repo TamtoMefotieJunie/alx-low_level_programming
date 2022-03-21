99be6fbcc79e37866146f714987367beea67a588
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-03-21 12:17:09 +0100

    pointers

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,16 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x05-pointers_arrays_strings directory.
- */
+#include <stdio.h>
 
-#include <stdlib.h>
-
-int _putchar(char c);
+void positive_or_negative(int i);
 void reset_to_98(int *n);
 
-#endif
+#endif /* MAIN_H */
