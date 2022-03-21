1baf65beca18acb1ddf3a31716daf0205e191df2
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-03-21 14:14:16 +0100

    pointers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,10 +11,12 @@
  */
 void print_rev(char *s)
 {
-int len = 0, index;
-while (s[index++])
+int len = 0,
+int i = 0;
+while (s[i] != '\0'])
 len++;
-for (index = len - 1; index >= 0; index--)
-_putchar(s[index]);
+i++;
+for (i = len - 1; i >= 0; i--)
+_putchar(s[i]);
 _putchar('\n');
 }
