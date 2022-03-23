66d8e98a83ca80db36f722ec6e4b5c7100d2810c
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-03-23 19:56:09 +0100

    pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,14 +14,14 @@ char *_strcat(char *dest, char *src)
   i = 0;
   while (dest[i] != '\0') 
   {
-    i++;
+    ++i;
   }
 
-  for (j = 0; src[j] != '\0'; j++, i++) 
+  for (j = 0; src[j] != '\0'; ++j, ++i) 
   {
-    *(dest + i) = *(src + j);
+    dest[i] = src[j];
   }
 
-  *(dest + i) = '\0';
+  dest[i] = '\0';
   return (dest)  
 }
