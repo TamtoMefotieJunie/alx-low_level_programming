1deb64d52fc560537540772a589b6cdf1e4f7613
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-03-24 16:14:17 +0100

    pointers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,20 +1,15 @@
+
 #include "main.h"
 
 /**
- * reverse_array: changes all lowercase letters of a string to uppercase
- *
- * @a: pointer to integer array
- * @n: size of array
+ * reverse_array - Reverses the content of an array of integers.
+ * @a: The array of integers to be reversed.
+ * @n: The number of elements in the array.
  */
-
 void reverse_array(int *a, int n)
 {
 int i;
-int tmp = 0;
-  for(i = 0; i < n; i++)
-  {
-    tmp = arr[i];
-    arr[i] = arr[n-1-i];
-    arr[n-1-i] = tmp;
-  }
+  for(i = n-1; i >= 0; i--)
+    _putchar(a[i]);
+    
 }
