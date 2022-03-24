26b3fcaf054ff72a65423c416f23325870b20693
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-03-24 15:52:44 +0100

    pointers

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,7 +3,7 @@
 /**
  * *string_toupper: changes all lowercase letters of a string to uppercase
  *
- * @s: pointer to character
+ * @s: pointer to character 
  * Return: char 
  */
 
@@ -12,6 +12,9 @@ char *string_toupper(char *s)
 int i;
 
 	for (i = 0; s[i] != '\0'; i++)
-    s[i] -= 32;
+    {
+    if (s[i] >= 'a' && s[i] <= 'z')
+    s[i] = s[i] - 32;
+    }
     return (s);
 }
