e6ac89ee39701dff1d22ccf063433a9e58430f3e
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-03-24 21:02:12 +0100

    pointers

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,10 +13,10 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
-for (i = 0; i < n; i++)
+for (i = 0; i <= n; i++)
 {
 dest[i] = src[i];
 }
-dest[n] = '\0';
+dest[i] = '\0';
 return (dest); 
 }
