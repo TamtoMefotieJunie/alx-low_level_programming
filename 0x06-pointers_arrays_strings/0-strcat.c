175721a663767beef7862cc7e0e807a4a1b1785f
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-03-25 20:17:59 +0100

    pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,16 +10,15 @@
 
 char *_strcat(char *dest, char *src)
 {
-unsigned int i = 0;
-unsigned int j = 0;
-	
-while (*(dest + i) != '\0')
-i++;
-while (*(src + j) != '\0')
-{
-*(dest + i) = *(src + j);
-i++;
-j++;
-}
-return (dest);
+	unsigned int i = 0;
+	unsigned int j = 0;
+	while (*(dest + i) != '\0')
+		i++;
+	while (*(src + j) != '\0')
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
+	return (dest);
 }
