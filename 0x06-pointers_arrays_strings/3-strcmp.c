393ebba64baf50d38f05e9bf35653cf5301c71ee
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-03-25 10:26:42 +0100

    pointers

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,22 +10,22 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-    int aux;
+int i = 0;
+int aux;
 	
-    while (*(s1 + i) != '\0')
-	{
-		if (*(s1 + i) > *(s2 + i))
+while (*(s1 + i) != '\0')
+{
+if (*(s1 + i) > *(s2 + i))
 		{
-			aux = *(s1 + i) - *(s2 + i);
-			return (aux);
+aux = *(s1 + i) - *(s2 + i);
+return (aux);
 		}
-		else if (*(s1 + i) < *(s2 + i))
-		{
-        	aux = *(s1 + i) - *(s2 + i);
-			return (aux);
-		}
-		i += 1;
-	}
-	return (0);
+else if (*(s1 + i) < *(s2 + i))
+{
+aux = *(s1 + i) - *(s2 + i);
+return (aux);
+}
+i += 1;
+}
+return (0);
 }
