f6e5412dee062a32762ea405788687c092de3156
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-03-25 12:10:40 +0100

    pointers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,10 +9,11 @@
 void reverse_array(int *a, int n)
 {
 int tmp, index;
-for (index = 0; index < n / 2; index++)
-{
-tmp = a[index];
-a[index] = a[n - 1 - index];
-a[n - 1 -index] = tmp;
-}
+
+	for (index = n - 1; index >= n / 2; index--)
+	{
+		tmp = a[n - 1 - index];
+		a[n - 1 - index] = a[index];
+		a[index] = tmp;
+	}
 }
