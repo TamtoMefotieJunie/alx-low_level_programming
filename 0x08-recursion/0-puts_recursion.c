f0adec6d5c95a857d5f9cc346bf8400b89b0ee71
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-03-30 16:41:57 +0200

    recursive function

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,8 +10,9 @@ void _puts_recursion(char *s)
 	if (*s)
 	{
 		_putchar(*s);
-        _puts_recursion(s + 1);
+		_puts_recursion(s + 1);
 	}
-    else
-	_putchar('\n');
+
+	else
+		_putchar('\n');
 }
