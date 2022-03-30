fdf8724b8db564deaa82b653459625ba27bc6a94
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-03-30 16:47:54 +0200

    recursive function

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,13 +6,15 @@
  *
  * Return: The length of the string.
  */
-
 int _strlen_recursion(char *s)
 {
-	int length = 0;
+	int len = 0;
 
 	if (*s)
-		length++;
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
 
-	return (_strlen_recursion(s + 1) + length);
+	return (len);
 }
