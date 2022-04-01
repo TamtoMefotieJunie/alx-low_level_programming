a1903224f13e1370d9b17ce097ff44d995a41d7e
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-04-01 20:25:30 +0200

    argc_argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,9 @@
  *
  * Return: Always o.
  */
-int main(int argc, char *argv[]) 
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
+
 	return (0);
 }
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,8 +7,9 @@
  *
  * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-    printf("%d\n", argc);
-    return (0);
+	printf("%d\n", argc);
+
+	return (0);
 }
