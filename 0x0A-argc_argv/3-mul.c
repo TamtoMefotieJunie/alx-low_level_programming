a0f213add81d9198d2d4cff6132af8002990d3ee
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-04-01 20:40:41 +0200

    argc_argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,12 +3,12 @@
 int main(int argc, char *argv[])
 {   int k, a, b;
     
-    if (argc < 3)
+    if (argc != 3)
     {
     printf("ERROR\n");
     return (1);
     }
-    
+
     a = atoi(argv[1]);
 	b = atoi(argv[2]);
     k = a * b;
