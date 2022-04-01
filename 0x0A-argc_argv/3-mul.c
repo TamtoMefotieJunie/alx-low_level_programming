108b3f246a9f33f999087a512895458ed7b112a8
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-04-01 20:42:51 +0200

    argc_argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Prints the multiplication of two numbers, followed by a new line.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: If the program receives two arguments - 0.
+ *         If the program does not receive two arguments - 1.
+ */
 int main(int argc, char *argv[])
 {   int k, a, b;
     
