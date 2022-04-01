551ee8a74cf1903a3c6ac84cee303ea7a420dc56
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-04-01 20:51:30 +0200

    argc_argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,19 +10,20 @@
  *         If the program does not receive two arguments - 1.
  */
 int main(int argc, char *argv[])
-{   int k, a, b;
-    
-    if (argc != 3)
-    {
-    printf("ERROR\n");
-    return (1);
-    }
+{
+	int num1, num2, prod;
 
-    a = atoi(argv[1]);
-	b = atoi(argv[2]);
-    k = a * b;
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    printf("%d\n", k);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	prod = num1 * num2;
 
-    return (0);
+	printf("%d\n", prod);
+
+	return (0);
 }
