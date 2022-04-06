79980172b00872ff8f910f43256fb37b4b766518
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-04-06 18:12:43 +0200

    malloc

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,29 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * main - Prints the addition of positive numbers,
- *        followed by a new line.
- * @argc: The number of arguments passed to the program.
- * @argv: An array of pointers to the arguments.
- *
- * Return: If one of the numbers contains symbols that are non-digits - 1.
- *         Otherwise - 0.
- */
+ * check_digit - checks if a given char is number or not
+ * @a: input char
+ * Return: int
+ **/
+
+int check_digit(char *a)
+{
+	int i, num, len;
+
+	i = 0;
+	num = 0;
+	len = strlen(a);
+	while (i < len)
+	{
+		if (a[i] < '0' || a[i] > '9')
+		{
+			return (-1);
+		}
+		else
+			num = num * 10 + (a[i] - '0');
+		i++;
+	}
+	return (num);
+}
+
+/**
+ * main -  program that adds positive numbers
+ * @argc: arguement count
+ * @argv: argument vector
+ * Return: int
+ **/
+
 int main(int argc, char *argv[])
 {
-   int num1, num2, sum;
+	int i, num, res;
 
-   if (argc == 0)
-   {
-   printf("0\n");
-   }
-   
-   num1 = atoi(argv[1]);
-   num2 = atoi(argv[2]);
-   sum = num1 + num2;
-
-   printf("%d\n", sum);
-
-   return (0);
+	res = 0;
+	for (i = 1; i < argc; i++)
+	{
+		num = check_digit(argv[i]);
+		if (num == -1)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		res += num;
+	}
+	printf("%d\n", res);
+	return (0);
 }
