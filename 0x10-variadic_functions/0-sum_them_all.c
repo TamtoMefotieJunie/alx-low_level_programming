ef43c76501dd78ec615837a1caa4fc8342ee7a9b
TamtoMefotieJunie <manuellatamto@gmail.com>
2022-04-14 20:43:11 +0200

    variadic

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,12 +11,16 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-va_list (args);
+va_list args;
 unsigned int i,
 unsigned int sum = 0; 
+
 va_start(args, n);
+
 for (i = 0; i < n; i++)
 sum += va_arg(args, int);
+
 va_end(args);
+
 return (sum);
 }
